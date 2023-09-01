9bcad11077a8b3fc75bb3b64243413c99b64086d
Yulin Zheng <lyra1225@gmail.com>
2023-08-31 17:41:40 -0700

    init commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,21 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Yulin Zheng on 8/31/23.
+//
+
+import FirebaseCore
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
