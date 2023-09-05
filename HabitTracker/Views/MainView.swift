56c52ae724d15d9b474d683a8c7adadd71c54764
Yulin Zheng <lyra1225@gmail.com>
2023-09-04 23:52:19 -0700

    navigate to login

--- HabitTracker/Views/MainView.swift
+++ HabitTracker/Views/MainView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            LoginView()
         }
-        .padding()
     }
 }
 
