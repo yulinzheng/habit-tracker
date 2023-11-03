00bff7c7999542e17dbf4aea902a5fe98a08f6cb
Yulin Zheng <lyra1225@gmail.com>
2023-11-03 16:35:48 -0700

    login button

---
+++ HabitTracker/Views/LoginButton.swift
@@ -0,0 +1,20 @@
+//
+//  HTButton.swift
+//  HabitTracker
+//
+//  Created by Yulin Zheng on 11/3/23.
+//
+
+import SwiftUI
+
+struct HTButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HTButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HTButton()
+    }
+}
